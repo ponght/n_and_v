755e66df1c8dcdb34cb768b38add543dd17d0910
ponght <ponght@gmail.com>
2019-02-27 20:22:11 +0700

    install vue and nuxt

---
+++ nandv/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="con-ceter">
+        <h1>About</h1>
+    </div>
+
+</template>
+
+
+<style>
+    .con-center {
+        text-align: center;
+        margin-top: 1 rem;
+    }
+</style>
---
+++ nandv/pages/hello.vue
@@ -0,0 +1,27 @@
+
+<template>
+<div class="con-center">
+
+    <h1> Hello {{ name }}</h1>
+    <input type="text" v-binnd:value="name">
+</div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                name: "Artdvp"
+            }
+        }
+    }
+</script>
+
+<style>
+    .con-center{
+        text-align: center ;
+        margin-top:  1rem ;
+    }
+</style>
+
+
